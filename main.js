b5c1251aa9dde940bcac83a7ccaab810280d2ce7
Dylan <dburston0@gmail.com>
2021-10-29 23:02:20 -0400

    jquery lab

---
+++ main.js
@@ -0,0 +1,25 @@
+  $('#button').on('click', function(){
+    var text = $('#addToList').val();
+    $('.skills').append($('<li><button class="delete">X</button>' + text + '</li>'));
+    $('.delete').click(function(e){
+        $(this).parent('li').remove();
+    })
+});
+
+function clearInput(){
+    if($('#addToList').val() !== ""){
+        $('#addToList') = "";
+    }
+}
+// I can't figure out how to remove the entry once the button is clicked 
+
+
+
+
+
+
+
+
+
+
+
